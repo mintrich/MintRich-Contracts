1fb41c3f8e830b66ce6a8d99102b0aa45c698f44
underwood2024 <frank@frankdeMacBook-Pro.local>
2024-06-29 18:16:04 +0800

    init code

--- src/MintRichNFTContract.sol
+++ src/MintRichNFTContract.sol
@@ -1,6 +1,34 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.20;
 
-contract MintRichNFTContract {
+import "lib/ERC721A-Upgradeable/contracts/ERC721AUpgradeable.sol";
+import "@openzeppelin/contracts/proxy/utils/Initializable.sol";
+
+contract MintRichNFTContract is ERC721AUpgradeable, Initializable {
+
+    uint256 public constant MAX_SUPPLY = 10000;
+
+    address public factoryAddress;
+
+    string private _baseURI;
+
+    /// @custom:oz-upgrades-unsafe-allow constructor
+    constructor(address _factoryAddress) {
+        factoryAddress = _factoryAddress;
+        _disableInitializers();
+    }
+
+    function initialize(
+        string calldata name_,
+        string calldata symbol_,
+        string calldata tokenBaseURI_
+    ) public initializerERC721A initializer {
+        __ERC721A_init(name_, symbol_);
+        _baseURI = tokenBaseURI_;
+    }
+
+    function _startTokenId() internal view virtual override returns (uint256) {
+        return 1;
+    }
 
 }
