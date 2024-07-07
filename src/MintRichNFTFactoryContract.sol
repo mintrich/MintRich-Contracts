d2ed98ea72cf8acb530548e32b377ff016902eea
underwood2024 <underwood@mint.rich>
2024-07-07 19:18:28 +0800

    mint rich contracts

--- src/MintRichNFTFactoryContract.sol
+++ src/MintRichNFTFactoryContract.sol
@@ -1,42 +1,55 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.20;
 
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 import "@openzeppelin/contracts/proxy/Clones.sol";
-import "./MintRichNFTContract.sol";
+import { MintRichNFTContract } from "./rich/MintRichNFTContract.sol";
+import { IMetadataRenderer } from "./metadata/IMetadataRenderer.sol";
 
-contract MintRichNFTFactoryContract is Ownable {
+contract MintRichNFTFactoryContract is ERC721Upgradeable, OwnableUpgradeable, UUPSUpgradeable {
 
     address public implementationAddress;
 
-    event RichCollectionCreated(address indexed owner, bytes32 collectionId, string name, string symbol, string tokenBaseURI);
+    address public metadataRenderer;
+
+    event MintRichCollectionCreated(address indexed owner, address indexed collectionAddress, bytes32 collectionId, string name, string symbol);
 
     error InvalidCaller();
 
-    constructor(
-        address _implementationAddress
-    ) Ownable(msg.sender) {
+    /// @custom:oz-upgrades-unsafe-allow constructor
+    constructor() {
+        _disableInitializers();
+    }
+
+    function initialize(address _implementationAddress) initializer public {
+        __ERC721_init("MintRich Owner", "MROwner");
+        __Ownable_init(_msgSender());
+        __UUPSUpgradeable_init();
+
         implementationAddress = _implementationAddress;
     }
 
     function createRichCollection(
         bytes32 collectionId,
-        string calldata name_,
-        string calldata symbol_,
-        string calldata tokenBaseURI_
+        string calldata name,
+        string calldata symbol,
+        bytes calldata information
     ) external {
         checkCaller(collectionId);
         address collection = Clones.cloneDeterministic(implementationAddress, collectionId);
 
         (bool success, bytes memory returnData) = collection.call(abi.encodeCall(
-            MintRichNFTContract.initialize, (name_, symbol_, tokenBaseURI_)));
+            MintRichNFTContract.initialize, (name, symbol, information)));
         if (!success) {
             assembly {
                 revert(add(returnData, 32), mload(returnData))
             }                
         }
 
-        emit RichCollectionCreated(msg.sender, collectionId, name_, symbol_, tokenBaseURI_);
+        _mint(msg.sender, uint256(uint160(collection)));
+        emit MintRichCollectionCreated(msg.sender, collection, collectionId, name, symbol);
     }
 
     function predictDeterministicAddress(bytes32 collectionId) external view returns (address) {
@@ -47,6 +60,21 @@ contract MintRichNFTFactoryContract is Ownable {
         if (address(bytes20(salt)) != msg.sender) {
             revert InvalidCaller();
         }
+    }
+
+    function _authorizeUpgrade(address newImplementation)
+        internal
+        onlyOwner
+        override
+    {}
+
+    function tokenURI(uint256 tokenId) public view override returns (string memory) {
+        _requireOwned(tokenId);
+        return IMetadataRenderer(metadataRenderer).tokenURI(tokenId);
+    }
+
+    function setMetadataRenderer(address _metadataRenderer) external onlyOwner {    
+        metadataRenderer = _metadataRenderer;
     }
 
 }
